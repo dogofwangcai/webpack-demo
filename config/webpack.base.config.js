66aa4310fcf1f99c3b849f5aa3a62ebd73e91b55
dingshang <547148127@qq.com>
2019-09-10 16:22:41 +0800

    ok

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HappyPack = require('happypack');
 const os = require('os');
 const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length });
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 function resolve(relatedPath) {
   return path.join(__dirname, relatedPath)
@@ -13,8 +14,8 @@ const webpackConfigBase = {
   },
   output: {
     path: resolve('../dist'),
-    filename: '[name].[hash:4].js',
-    chunkFilename: 'chunks/[name].[hash:4].js',
+    filename: '[name].[contenthash].js',
+    chunkFilename: 'chunks/[name].[contenthash].js',
   },
   resolve: {
     extensions: ['.js', '.json'],
@@ -29,14 +30,43 @@ const webpackConfigBase = {
     rules: [
       {
         test: /\.js[x]?$/,
-        exclude: /node_modules/,
+        exclude: /node_modules/, 
         include: [resolve('../src')],
         //把对.js 的文件处理交给id为happyBabel 的HappyPack 的实例执行
         loader: 'happypack/loader?id=happyBabel',
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+         exclude: /node_modules/, 
+        include: [resolve('../src/images')],
+        loader: 'url',
+        options: {
+          limit: 8192,
+          name: 'img/[name].[hash:4].[ext]'
+        }
+      },
+      {
+        test: /\.(woff|eot|ttf|svg|gif)$/,
+        loader: 'url',
+        options: {
+          limit: 8192,
+          name: 'font/[name].[hash:4].[ext]'
+        }
+      },
+       {
+        test: /\.css|less$/,  // 用正则去匹配要用该 loader 转换的 CSS 文件
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
       }
     ],
   },
   plugins:[
+      new MiniCssExtractPlugin({
+      　　filename: "[name].[contenthash:8].css",
+          chunkFilename: "chunks/[name].[contenthash:8].css"
+   　 }),
       new HappyPack({
         //用id来标识 happypack处理那里类文件
         id: 'happyBabel',
@@ -54,6 +84,25 @@ const webpackConfigBase = {
         manifest: require('../src/dll/vendor.manifest.json'), 
         context: __dirname, 
       }),
-  ]
+  ],
+  optimization: {
+    minimize:'process.env.NODE_ENV' === 'production' ? true : false, // 开发环境不压缩
+    splitChunks: {
+        chunks: "initial", // 共有三个值可选：initial(初始模块)、async(按需加载模块)和all(全部模块)
+        minSize: 30000, // 模块超过30k自动被抽离成公共模块
+        minChunks: 3, // 模块被引用>=1次，便分割
+        maxAsyncRequests: 5,  // 异步加载chunk的并发请求数量<=5
+        maxInitialRequests: 3, // 一个入口并发加载的chunk数量<=3
+        name: true, // 默认由模块名+hash命名，名称相同时多个模块将合并为1个，可以设置为function
+        automaticNameDelimiter: '~', // 命名分隔符
+        cacheGroups: { // 缓存组，会继承和覆盖splitChunks的配置
+            default: { // 模块缓存规则，设置为false，默认缓存组将禁用
+                minChunks: 1, // 模块被引用>=2次，拆分至vendors公共模块
+                priority: 20, // 优先级
+                reuseExistingChunk: true, // 默认使用已有的模块
+            },
+        }
+    }
+  }
 };
 module.exports = webpackConfigBase
--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,42 +1,38 @@
 var path = require('path')
 var webpack = require('webpack');
-const HappyPack = require('happypack');
-const os = require('os');
-const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length });
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin')
-const Copy = require('copy-webpack-plugin')
 const webpackBaseConfig = require('./webpack.base.config')
-const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
-const merge = require('webpack-merge')
+const merge = require('webpack-merge');
+const port = 9090;
 function resolve(relatedPath) {
   return path.join(__dirname, relatedPath)
 }
 const webpackProd = {
-  mode:'production',
+  mode:'development',
   output: {
-    publicPath: './',
+    //publicPath: './',
+    filename: '[name].[hash].js',
+  },
+  devtool:'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: resolve('../src'),
+    // historyApiFallback: false,
+    hot: true,
+    host: 'localhost',
+    port: port,
+    open:true,
+    overlay:{ 
+      warnings: false, 
+      errors: true 
+    },
+    watchOptions: {
+      poll: true,
+    }
   },
   plugins:[
-    new HappyPack({
-        //用id来标识 happypack处理那里类文件
-        id: 'happyBabel',
-        //如何处理  用法和loader 的配置一样
-        loaders: [{
-          loader: 'babel?cacheDirectory=true',
-        }],
-        //代表共享进程池，即多个 HappyPack 实例都使用同一个共享进程池中的子进程去处理任务，以防止资源占用过多。
-        threadPool: happyThreadPool,
-        //允许 HappyPack 输出日志
-        verbose: true,
-    }),
-    new webpack.DllReferencePlugin({
-      manifest: require('../src/dll/vendor.manifest.json'), 
-      context: __dirname, 
-    }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-      IS_DEVELOPMETN: false,
+      'process.env.NODE_ENV': JSON.stringify('development'),
+      IS_DEVELOPMETN: true,
     }),
     // 将打包后的资源注入到html文件内    
     new HtmlWebpackPlugin({
@@ -48,24 +44,7 @@ const webpackProd = {
         './dll/vendor.dll.js', 
       ],
     }),
-    new Copy([
-      { from: './src/dll', to: '../dist/dll' },
-    ]),
-    new CleanWebpackPlugin({
-      root: path.join(__dirname, '../'),
-      verbose:false,
-      // exclude:['img']//不删除img静态资源
-    }),
-    /* 多核压缩代码 */
-    new ParallelUglifyPlugin({
-      cacheDir: '.cache/',
-      uglifyJS:{
-        output: {
-          comments: false
-        },
-        warnings: false
-      }
-    }),
+    
    ]
 };
 
